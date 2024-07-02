8b334217ed17458f448ec4058ba426a391aa3fb7
Bhawna <bhawnathreja19@gmail.com>
2024-07-02 13:41:05 +0800

    push

---
+++ Vtiger_DDT/src/test/java/SampleTest1Test.java
@@ -0,0 +1,14 @@
+
+
+import org.testng.annotations.Test;
+
+
+public class SampleTest1Test {
+	
+	@Test
+	public void test() {
+		
+		System.out.println("Test1");
+	}
+
+}
---
+++ Vtiger_DDT/src/test/java/SampleTest2.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+
+public class SampleTest2 {
+	
+	@Test
+	public void test2() {
+		
+		System.out.println("test2");
+	}
+
+}
